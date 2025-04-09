76769bd89c505b67397d94cf0398734f454e39b5
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-09 15:30:23 +0530

    created task repository

---
+++ taskmanager/src/main/java/lk/ijse/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package lk.ijse.taskmanager.repository;
+
+
+import lk.ijse.taskmanager.entity.TaskEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+}
