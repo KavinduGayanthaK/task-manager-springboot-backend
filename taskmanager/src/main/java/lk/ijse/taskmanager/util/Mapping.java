c49178ed796fa6c35dd36677af17bcd81857f3f5
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-09 14:23:29 +0530

    created mapping class

---
+++ taskmanager/src/main/java/lk/ijse/taskmanager/util/Mapping.java
@@ -0,0 +1,20 @@
+package lk.ijse.taskmanager.util;
+
+import lk.ijse.taskmanager.dto.TaskDTO;
+import lk.ijse.taskmanager.entity.TaskEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+
+
+    private final ModelMapper modelMapper;
+
+    public Mapping(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+
+
+}
