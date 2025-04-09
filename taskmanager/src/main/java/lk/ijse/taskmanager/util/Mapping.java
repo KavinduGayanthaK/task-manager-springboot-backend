27a2ebceaa1fcb230773f07cd0eb78e142825d60
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-09 16:12:47 +0530

    created task entity list convert to task dto list

--- taskmanager/src/main/java/lk/ijse/taskmanager/util/Mapping.java
+++ taskmanager/src/main/java/lk/ijse/taskmanager/util/Mapping.java
@@ -5,6 +5,9 @@ import lk.ijse.taskmanager.entity.TaskEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class Mapping {
 
@@ -22,5 +25,9 @@ public class Mapping {
 
     public TaskDTO toTaskDTO(TaskEntity taskEntity) {
         return modelMapper.map(taskEntity, TaskDTO.class);
+    }
+
+    public List<TaskDTO> toTaskDTOList(List<TaskEntity> taskEntities) {
+        return taskEntities.stream().map(this::toTaskDTO).collect(Collectors.toList());
     }
 }
